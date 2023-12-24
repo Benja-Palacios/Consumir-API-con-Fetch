e86f5434d34d87bd8d69cb08b2a2e7c9f32c3076
Benjamin <benjaminmpalacios@gmail.com>
2023-12-23 23:13:43 -0600

    Crear el html basico para mostrar los datos

--- consumirAPI.js
+++ consumirAPI.js
@@ -1,14 +1,43 @@
 async function obtenerDatos(){
     try {
-        const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+        const res = await fetch('https://jsonplaceholder.typicode.com/users');
         if (!res.ok) {
             throw new Error('❎')
         }
         const data = await res.json();
         console.log(data);
+        insertarDatosEnTabla(data);
     } catch (err) {
-        console.error(err)
+        console.error(err);
     }
 }
+
+function insertarDatosEnTabla(data) {
+    const tabla = document.getElementById("tableUsers");
+    const thead = tabla.querySelector('thead');
+    const tbody = tabla.querySelector('tbody');
+
+    // Limpiar el contenido anterior de la tabla
+    thead.innerHTML = '';
+    tbody.innerHTML = '';
+
+    // Crear encabezados basados en las propiedades del primer objeto
+    const encabezados = Object.keys(data[0]);
+    const encabezadosHTML = encabezados.map(
+        encabezado => `<th>${encabezado}</th>`
+    ).join('');
+    thead.innerHTML = `<tr>${encabezadosHTML}</tr>`;
+
+    // Iterar sobre los datos y agregar filas a la tabla
+    data.forEach(usuario => {
+        const fila = document.createElement('tr');
+        const celdasHTML = encabezados.map(
+            encabezado => `<td>${usuario[encabezado]}</td>`
+        ).join('');
+        fila.innerHTML = celdasHTML;
+        tbody.appendChild(fila);
+    });
+}
+
 
 obtenerDatos();
